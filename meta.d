d6d03190f0d7862efbecd6941a868036261844dc
k-hara <k.hara.pg+dev@gmail.com>
2010-04-29 00:39:40 +0900

    delete debug messages, and change code style like phobos.

--- meta.d
+++ meta.d
@@ -3,63 +3,44 @@
 public import std.typetuple;
 public import std.traits;
 
+import my_demangle : Demangle;
 
 
-import my_demangle : Demangle;//std.demangle;
 template ToLongString(T)
 {
-//	pragma(msg, T.stringof);
-	static if( Demangle!T.length >= 6 && Demangle!T[0..6] == "class " ){
+	static if (Demangle!T.length >= 6 && Demangle!T[0..6] == "class ")
+	{
 		enum ToLongString = Demangle!T[6..$];
-	}else{
+	}
+	else static if (Demangle!T.length >= 7 && Demangle!T[0..7] == "struct ")
+	{
+		enum ToLongString = Demangle!T[7..$];
+	}
+	else static if (Demangle!T.length >= 5 && Demangle!T[0..5] == "enum ")
+	{
+		enum ToLongString = Demangle!T[5..$];
+	}
+	else static if (Demangle!T.length >= 8 && Demangle!T[0..8] == "typedef ")
+	{
+		enum ToLongString = Demangle!T[8..$];
+	}
+	else
+	{
 		enum ToLongString = Demangle!T;
 	}
 }
-static assert(ToLongString!int == "int");
-
-
-/+
-template ToString(T){
-	enum ToString = T.stringof;
-}
-
-template isType(A)		{ enum isType = true;  }
-template isType(alias A){ enum isType = false; }
-template ToString(T)
+unittest
 {
-	enum ToString = FullName!T;//T.stringof;
+	static assert(ToLongString!int == "int");
 }
-template ToString(alias T)
-{
-	enum ToString = T.stringof;
-}
-template TupleToString(T...)
-{
-	static if( T.length == 0 ){
-		enum TupleToString = "";
-	}else{
-		static if( is(typeof(T[0])) ){
-			enum TupleToString = ToString!(T[0]) ~ (T.length>1?", ":"") ~ TupleToString!(T[1..$]);
-		}else{
-			enum TupleToString = ToString!(T[0]) ~ (T.length>1?", ":"") ~ TupleToString!(T[1..$]);
-		}
-	}
-}
-static assert(TupleToString!(int) == "int");
-static assert(TupleToString!("STR") == `"STR"`);
-static assert(TupleToString!(int, "STR") == `int, "STR"`);
-static assert(TupleToString!(int, float) == "int, float");
-static assert(TupleToString!(2, 3) == "2, 3");
-static assert(TupleToString!("STR", "VAL") == `"STR", "VAL"`);
-+/
 
 
 template LeastUnsignedType(size_t N)
 {
-	     static if( N <= ubyte .max ) alias ubyte	LeastUnsignedType;
-	else static if( N <= ushort.max ) alias ushort	LeastUnsignedType;
-	else static if( N <= uint  .max ) alias uint	LeastUnsignedType;
-	else static if( N <= ulong .max ) alias ulong	LeastUnsignedType;
+	     static if (N <= ubyte .max) alias ubyte	LeastUnsignedType;
+	else static if (N <= ushort.max) alias ushort	LeastUnsignedType;
+	else static if (N <= uint  .max) alias uint		LeastUnsignedType;
+	else static if (N <= ulong .max) alias ulong	LeastUnsignedType;
 	else static assert(0, "tag-size is too big");
 	
 }
@@ -68,11 +49,16 @@ template LeastUnsignedType(size_t N)
 
 template staticCat(V...)
 {
-	static if( V.length == 0 ){
+	static if (V.length == 0)
+	{
 		enum staticCat = "";
-	}else static if( isSomeString!(typeof(V[0])) ){
+	}
+	else static if (isSomeString!(typeof(V[0])))
+	{
 		enum staticCat = V[0] ~ staticCat!(V[1..$]);
-	}else{
+	}
+	else
+	{
 		static assert(0);
 	}
 }
@@ -81,47 +67,61 @@ static assert(staticCat!("A", "B", "C") == "ABC");
 
 
 // 整数範囲を指定し、テンプレート関数からタプルを生成する
-template generateTuple(int Begin, int End, alias F) if( Begin <= End )
+template generateTuple(int Begin, int End, alias F) if (Begin <= End)
 {
-	static if( Begin == End ){
+	static if (Begin == End)
+	{
 		alias TypeTuple!() generateTuple;
-	}else{
+	}
+	else
+	{
 		alias TypeTuple!(F!(Begin), generateTuple!(Begin+1, End, F)) generateTuple;
 	}
 }
 
-template BinaryFun(string Code){
-	template BinaryFun(alias A, alias B){
+template BinaryFun(string Code)
+{
+	template BinaryFun(alias A, alias B)
+	{
 		enum BinaryFun = mixin(Code);
 	}
 }
 
-// reduce
 private template staticReduceEnv(alias Init, alias F, T...)
 {
-	template Temp(alias T){
+	template Temp(alias T)
+	{
 		alias T Res;
 	}
-	template Engine(alias Tmp, T...){
+	template Engine(alias Tmp, T...)
+	{
 		//pragma(msg, "Engine: (", Tmp.Res, "), ", T);
-		static if( T.length == 0 ){
+		static if (T.length == 0)
+		{
 			alias Tmp.Res Engine;
-		}else{
+		}
+		else
+		{
 			alias Engine!(Temp!(F!(Tmp.Res, T[0])), T[1..$]) Engine;
 		}
 	}
 	alias Engine!(Temp!(Init), T) Res;
 }
+/// reduce
 template staticReduce(alias F, alias Init, T...)
 {
-	static if( isSomeString!(typeof(F)) ){
+	static if (isSomeString!(typeof(F)))
+	{
 		alias staticReduceEnv!(Init, BinaryFun!F, T).Res staticReduce;
-	}else{
+	}
+	else
+	{
 		alias staticReduceEnv!(Init, F, T).Res staticReduce;
 	}
 }
 
 
-unittest{
+unittest
+{
 	static assert(staticReduce!(q{A==""?B:A~", "~B}, "", TypeTuple!("AAA", "BBB", "CCC")) == "AAA, BBB, CCC");
 }
--- tag_union.d
+++ tag_union.d
@@ -2,11 +2,11 @@
 
 public import tuple_tie;
 
-version(unittest){
-	import std.stdio : wr=writefln;
-}else{
-	void wr(T...)(T args){}
-}
+//version(unittest){
+	import std.stdio : pp=writefln;
+//}else{
+//	void pp(T...)(T args){}
+//}
 
 
 template TagUnion(T...)
@@ -15,59 +15,89 @@ private:
 	import meta;
 	import std.conv : to;
 	
-	/// BUG ここに定義しておかないとdmdが死ぬ
+	/// BUG shuld be inner template, though dmd dead
 	template ToLongString(T)
 	{
-		static if( Demangle!T.length >= 6 && Demangle!T[0..6] == "class " ){
+		static if (Demangle!T.length >= 6 && Demangle!T[0..6] == "class ")
+		{
 			enum ToLongString = Demangle!T[6..$];
-		}else static if( Demangle!T.length >= 7 && Demangle!T[0..7] == "struct " ){
+		}
+		else static if (Demangle!T.length >= 7 && Demangle!T[0..7] == "struct ")
+		{
 			enum ToLongString = Demangle!T[7..$];
-		}else static if( Demangle!T.length >= 5 && Demangle!T[0..5] == "enum " ){
+		}
+		else static if (Demangle!T.length >= 5 && Demangle!T[0..5] == "enum ")
+		{
 			enum ToLongString = Demangle!T[5..$];
-		}else static if( Demangle!T.length >= 8 && Demangle!T[0..8] == "typedef " ){
+		}
+		else static if (Demangle!T.length >= 8 && Demangle!T[0..8] == "typedef ")
+		{
 			enum ToLongString = Demangle!T[8..$];
-		}else{
+		}
+		else
+		{
 			enum ToLongString = Demangle!T;
 		}
 	}
 
-	
-	template GetNames(E...){
-		static if( E.length == 0 ){
+	template GetNames(E...)
+	{
+		static if (E.length == 0)
+		{
 			alias TypeTuple!() GetNames;
-		}else static if( is(typeof(E[0])) && isSomeString!(typeof(E[0])) ){
+		}
+		else static if (is(typeof(E[0])) && isSomeString!(typeof(E[0])))
+		{
 			alias TypeTuple!(E[0], GetNames!(E[1..$])) GetNames;
-		}else{
+		}
+		else
+		{
 			alias GetNames!(E[1..$]) GetNames;
 		}
 	}
-	template GetFieldsStart(size_t N){
+	template GetFieldsStart(size_t N)
+	{
 		enum GetFieldsStart = staticIndexOf!(GetNames!T[N], T) + 1;
 	}
-	template GetFieldsEnd(size_t N){
-		static if( N==GetNames!T.length-1 )	enum GetFieldsEnd = T.length;
-		else								enum GetFieldsEnd = staticIndexOf!(GetNames!T[N+1], T);
+	template GetFieldsEnd(size_t N)
+	{
+		static if (N==GetNames!T.length-1)
+		{
+			enum GetFieldsEnd = T.length;
+		}
+		else
+		{
+			enum GetFieldsEnd = staticIndexOf!(GetNames!T[N+1], T);
+		}
 	}
 	
-  //base parameters
+  // basic parameters
 	alias GetNames!T TyconTags;
-	enum  TyconCnt = TyconTags.length;
-	template TyconTag(size_t N)	{ enum TyconTag = GetNames!T[N]; }
-	template TyconSig(size_t N)	{ alias TypeTuple!(T[GetFieldsStart!N .. GetFieldsEnd!N]) TyconSig; }
+	enum TyconCnt = TyconTags.length;
+	template TyconTag(size_t N)
+	{
+		enum TyconTag = GetNames!T[N];
+	}
+	template TyconSig(size_t N)
+	{
+		alias TypeTuple!(T[GetFieldsStart!N .. GetFieldsEnd!N]) TyconSig;
+	}
 	
 	
 	public enum MakeTyconTags =
 		"enum Tag:"~(LeastUnsignedType!TyconCnt).stringof~"{ "
 			~staticReduce!(q{A==""?B~"=0":A~", "~B}, "", TyconTags)~" "
 		"}";
-	//pragma(msg, MakeTyconTags);
 	
-	template MakeTyconTypeField(size_t N){
-		template MakeTyconTypeField(size_t I){
+	template MakeTyconTypeField(size_t N)
+	{
+		template MakeTyconTypeField(size_t I)
+		{
 			enum MakeTyconTypeField = ToLongString!(TyconSig!N[I])~" _"~to!string(I);
 		}
 	}
-	template MakeTyconType(size_t N){
+	template MakeTyconType(size_t N)
+	{
 		enum MakeTyconType =
 			"static struct "~TyconTag!N~"_T{ "
 				"Tag tag; "
@@ -76,9 +106,9 @@ private:
 	}
 	public enum MakeTyconTypes =
 		staticReduce!(q{A==""?B:A~"\n"~B}, "", generateTuple!(0, TyconCnt, MakeTyconType));
-	//pragma(msg, MakeTyconTypes);
 	
-	template MakeTyconData(size_t N){
+	template MakeTyconData(size_t N)
+	{
 		enum MakeTyconData = TyconTag!N~"_T data"~to!string(N)~";";
 	}
 	public enum MakeTyconDatas =
@@ -86,7 +116,6 @@ private:
 			"Tag tag; "
 			~staticReduce!(q{A==""?B:A~" "~B}, "", generateTuple!(0, TyconCnt, MakeTyconData))~
 		" }";
-	//pragma(msg, MakeTyconDatas);
 
 	template MakeCtor(size_t N){
 		enum MakeCtor =
@@ -94,55 +123,53 @@ private:
 	}
 	public enum MakeCtors =
 		staticReduce!(q{A==""?B:A~"\n"~B}, "", generateTuple!(0, TyconCnt, MakeCtor));
-	//pragma(msg, MakeCtors);
 	
 	template MakeTycon(size_t N){
 		enum MakeTycon =
 			`static auto `~TyconTag!N~`(U...)(U args){`															"\n"
-			`	static if( is(U == TypeTuple!(`
-						~staticReduce!(q{A==""?B:A~", "~B}, "", staticMap!(ToLongString, TyconSig!N))~	`)) ){`	"\n"
-		//	`		pragma(msg, "make_Tycon: `~TyconTag!N~`, ", U);`											"\n"
+			`	static if (is(U == TypeTuple!(`
+						~staticReduce!(q{A==""?B:A~", "~B}, "", staticMap!(ToLongString, TyconSig!N))~	`)))`	"\n"
+			`	{`																								"\n"
 			`		return new typeof(this)(`~TyconTag!N~`_T(Tag.`~TyconTag!N~`, args));`						"\n"
-			`	}else`																							"\n"
-			`	static if( Tie!U.isMatchingTuple!(`
-						~staticReduce!(q{A==""?B:A~", "~B}, "", staticMap!(ToLongString, TyconSig!N))~	`) ){`	"\n"
-		//	`		pragma(msg, "make_Tie: `~TyconTag!N~`, ", U);`												"\n"
+			`	}`																								"\n"
+			`	else static if (Tie!U.isMatchingTuple!(`
+						~staticReduce!(q{A==""?B:A~", "~B}, "", staticMap!(ToLongString, TyconSig!N))~	`))`	"\n"
+			`	{`																								"\n"
 			`		return tie(Tag.`~TyconTag!N~`, args);`														"\n"
-		//	`		pragma(msg, "TagUnion.opTieMatch: ret_t=", typeof(return));`								"\n"
-			`	}else{`																							"\n"
+			`	}`																								"\n"
+			`	else`																							"\n"
+			`	{`																								"\n"
 			`		static assert(0);`																			"\n"
 			`	}`																								"\n"
 			`}`;
 	}
 	public enum MakeTycons =
 		staticReduce!(q{A==""?B:A~"\n"~B}, "", generateTuple!(0, TyconCnt, MakeTycon));
-	//pragma(msg, MakeTycons);
 	
 	
 	template MakeTieMatchCase(size_t N){
 		enum MakeTieMatchCase =
-			`	case Tag.`~TyconTag!N~`:`															"\n"
-		//	`		pragma(msg, "  N=`~to!string(N)~`, Tie!(", U, ").isMatchingTuple = ", Tie!U.isMatchingTuple!(Tag, TyconSig!`~to!string(N)~`));`		"\n"
-			`		static if( Tie!U.isMatchingTuple!(Tag, TyconSig!`~to!string(N)~`) ){`			"\n"
-		//	`			pragma(msg, "  N=`~to!string(N)~`, isMatchingTuple!(Tag, ", TyconSig!`~to!string(N)~`, ") = true");`				"\n"
-			`			return tie = tuple(data`~to!string(N)~`.tupleof);`							"\n"
-			`		}else{`																			"\n"
-		//	`			pragma(msg, "  N=`~to!string(N)~`, isMatchingTuple!(Tag, ", TyconSig!`~to!string(N)~`, ") = false");`				"\n"
-			`			return false;`																"\n"
+			`	case Tag.`~TyconTag!N~`:`														"\n"
+			`		static if (Tie!U.isMatchingTuple!(Tag, TyconSig!`~to!string(N)~`))`			"\n"
+			`		{`																			"\n"
+			`			return tie = tuple(data`~to!string(N)~`.tupleof);`						"\n"
+			`		}`
+			`		else`																		"\n"
+			`		{`																			"\n"
+			`			return false;`															"\n"
 			`		}`;
 	}
-	template MakeTieMatchInstanceIf(size_t N){
+	template MakeTieMatchInstanceIf(size_t N)
+	{
 		enum MakeTieMatchInstanceIf =
 			`Tie!U.isMatchingTuple!(Tag, TyconSig!`~to!string(N)~`)`;
 	}
 	public enum MakeTieMatch = 
 		`bool opTieMatch(U...)(ref Tie!U tie){`													"\n"
-		`	static if( !(`
+		`	static if (!(`
 				~staticReduce!(q{A==""?""~B:A~" || "~B}, "", generateTuple!(0, TyconCnt, MakeTieMatchInstanceIf))~
-			`) ){`																				"\n"
-	//	`		pragma(msg, "opTieMatch instantiation ok");`									"\n"
-	//	`	}else{`																				"\n"
-	//	`		pragma(msg, "opTieMatch instantiation failed");`								"\n"
+			`))`																				"\n"
+		`	{`																					"\n"
 		`		static assert(0);`																"\n"
 		`	}`																					"\n"
 		"	\n"
@@ -150,7 +177,6 @@ private:
 		~staticReduce!(q{A==""?B:A~"\n"~B}, "", generateTuple!(0, TyconCnt, MakeTieMatchCase))~	"\n"
 		`	}`																					"\n"
 		`}`;
-	//pragma(msg, MakeTieMatch);
 	
 	
 	enum MakeTyconAlias =
@@ -165,28 +191,30 @@ private:
 		`				TypeTuple!(`~staticReduce!(q{A==""?"\""~B:A~"\", \""~B}, "", TyconTags)~`")[N]`			"\n"
 		`			~";";`																						"\n"
 		`}`																										"\n"
-		`template Tycons(){`																					"\n"
+		`template Tycons()`																						"\n"
+		`{`																										"\n"
 		`	enum Tycons =`																						"\n"
 		`		staticReduce!(q{A==""?B:A~"\n"~B}, "", generateTuple!(0, `~to!string(TyconCnt)~`, MakeAlias));`	"\n"
 		`}`;
 
 public:
   private:
-	mixin(/*TagUnionImpl!T.*/MakeTyconTags);	//pragma(msg, /*TagUnionImpl!T.*/MakeTyconTags);
-	mixin(/*TagUnionImpl!T.*/MakeTyconTypes);	//pragma(msg, /*TagUnionImpl!T.*/MakeTyconTypes);
-	mixin(/*TagUnionImpl!T.*/MakeTyconDatas);	//pragma(msg, /*TagUnionImpl!T.*/MakeTyconDatas);
-	mixin(/*TagUnionImpl!T.*/MakeTieMatch);		//pragma(msg, /*TagUnionImpl!T.*/MakeTieMatch);
+	mixin(MakeTyconTags);
+	mixin(MakeTyconTypes);
+	mixin(MakeTyconDatas);
+	mixin(MakeTieMatch);
   public:
-	mixin(/*TagUnionImpl!T.*/MakeCtors);		//pragma(msg, /*TagUnionImpl!T.*/MakeCtors);
-	mixin(/*TagUnionImpl!T.*/MakeTycons);		//pragma(msg, /*TagUnionImpl!T.*/MakeTycons);
-	mixin(/*TagUnionImpl!T.*/MakeTyconAlias);	//pragma(msg, /*TagUnionImpl!T.*/MakeTyconAlias);
+	mixin(MakeCtors);
+	mixin(MakeTycons);
+	mixin(MakeTyconAlias);
 }
 
 
 //==============================================================================
 
 alias int IntT;
-class temp{
+class temp
+{
 	static class Label{}
 	static class Temp{}
 }
@@ -225,8 +253,9 @@ mixin(Stm.Tycons!());
 static assert(__traits(classInstanceSize, Stm) == 28);
 
 
-unittest{
-	wr("tag_union.unittest");
+unittest
+{
+	pp("tag_union.unittest");
 	
 	auto l = new temp.Label();
 	
@@ -247,10 +276,13 @@ unittest{
 	{	Stm s_;
 		Exp e_;
 		// pattern match(OK)
-		if( ESEQ(&s_, &e_) = eseq ){
+		if (ESEQ(&s_, &e_) = eseq)
+		{
 			assert(s is s_);
 			assert(e is e_);
-		}else{
+		}
+		else
+		{
 			assert(0);
 		}
 		s_ = null, e_ = null;
@@ -259,20 +291,25 @@ unittest{
 		static assert(!__traits(compiles, ESEQ(&s_, &e_) = tuple(7, s, e)));
 		
 		// tag pattern match(OK)
-		if( ESEQ(&s_, e) = eseq ){
+		if (ESEQ(&s_, e) = eseq)
+		{
 			assert(s is s_);
-		}else{
+		}
+		else
+		{
 			assert(0);
 		}
 		
 		// tag pattern match(NG)
-		if( MEM(&e_) = eseq ){
+		if (MEM(&e_) = eseq)
+		{
 			assert(0);
 		}
 		
 		// value pattern match(NG)
 		s_ = null, e_ = null;
-		if( ESEQ(&s_, e_) = eseq ){
+		if (ESEQ(&s_, e_) = eseq)
+		{
 			assert(0);
 		}
 		
@@ -284,14 +321,17 @@ unittest{
 		
 		// raw match
 		Exp.Tag t;
-		if( tie(&t, &s_, &e_) = eseq ){
+		if (tie(&t, &s_, &e_) = eseq)
+		{
 			assert(t == Exp.Tag.ESEQ);
 			assert(s is s_);
 			assert(e is e_);
-		}else{
+		}
+		else
+		{
 			assert(0);
 		}
 	}
 	
-	wr("-> test ok");
+	pp("-> test ok");
 }
